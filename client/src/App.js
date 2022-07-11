694a598cec50cd1feefacec9b794f753be38a5c2
gabefando <gabe.fonseca.04@gmail.com>
2022-07-11 03:41:59 -0400

    created all inputs

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-    </div>
+      <label>Client/company name:</label>
+      <input type="text"/>
+      <label>Phone number:</label>
+      <input type="number"/>
+      <label>Address:</label>
+      <input type="text"/>
+      <label>Type of service:</label>
+      <select id="serviceType">
+        <option selected="selected">Choose...</option>
+        <option>One time service</option>
+        <option>Repeated service</option>
+      </select>
+      <label>Price (per service):</label>
+      <input type="number"/>
+      <label>Service description:</label>
+      <textarea type="text"/>
+  </div>
   );
 }
 
