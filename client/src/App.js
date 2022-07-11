c3b2db2dc95d63ad9d309a035ecd0f09a545ad29
gabefando <gabe.fonseca.04@gmail.com>
2022-07-11 07:19:51 -0400

    button click

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,51 @@ function App() {
   const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
   const [type, setService] = useState('');
-  const [price, setPrice] = useState('');
+  const [price, setPrice] = useState(0);
   const [desc, setDesc] = useState('');
 
+  const displayInfo = () => {
+    console.log(name + phone + address + type + price + desc);
+  };
   return (
     <div className="App">
       <div className='information'>
         <label>Client/company name:</label>
-        <input type="text"/>
+        <input type="text" onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
         <label>Phone number:</label>
-        <input type="text"/>
+        <input type="text" onChange={(event) => {
+              setPhone(event.target.value);
+            }}
+          />
         <label>Address:</label>
-        <input type="text"/>
+        <input type="text"onChange={(event) => {
+              setAddress(event.target.value);
+            }}
+          />
         <label>Type of service:</label>
-        <select id="serviceType">
-          <option selected="selected">Choose...</option>
+        <select onChange={(event) => {
+              setService(event.target.value);
+            }}>
+          <option>Choose...</option>
           <option>One time service</option>
-          <option>Repeated service</option>
+          <option>Repeated service (weekly)</option>
+          <option>Repeated service (bi-weekly)</option>
+          <option>Repeated service (monthly)</option>
         </select>
         <label>Price (per service):</label>
-        <input type="number"/>
+        <input type="number" onChange={(event) => {
+              setPrice(event.target.value);
+            }}
+          />
         <label>Service description:</label>
-        <textarea type="text"/>
-        <button>Add Client</button>
+        <textarea type="text" onChange={(event) => {
+              setDesc(event.target.value);
+            }}
+          />
+        <button onClick={displayInfo}>Add Client</button>
       </div>
   </div>
   );
